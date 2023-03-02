a18297b8eabbbee875a780961ce61a9d5ecad59a
teitonny <tonnytei4@gmail.com>
2023-03-02 19:29:35 +0300

    added tests to veryfy operate function

--- src/tests/operate.test.js
+++ src/tests/operate.test.js
@@ -4,4 +4,16 @@ describe('Verify operate function', () => {
   test('Should return 2 when 1 + 1', () => {
     expect(operate(1, 1, '+')).toBe('2');
   });
+  test('Should return 2 when 5 - 3', () => {
+    expect(operate(5, 3, '-')).toBe('2');
+  });
+  test('Should return 4 when 2 x 2', () => {
+    expect(operate(2, 2, 'x')).toBe('4');
+  });
+  test('Should return 2 when 4/2', () => {
+    expect(operate(4, 2, '÷')).toBe('2');
+  });
+  test('Should return 2 when 4 % 3', () => {
+    expect(operate(4, 3, '%')).toBe('2');
+  });
 });
