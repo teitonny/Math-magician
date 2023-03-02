5797a0aea140e620f15684deef132175d3b7f3db
krisbobo <kiyongbohbo@gmail.com>
2023-03-02 17:12:42 +0100

    Add test

--- src/tests/Home.test.js
+++ src/tests/Home.test.js
@@ -1,2 +1,8 @@
-import Home from "../components/Home";
+import React from 'react';
+import Renderer from 'react-test-renderer';
+import Home from '../components/Home';
 
+it('renders correctly', () => {
+  const tree = Renderer.create(<Home />).toJSON();
+  expect(tree).toMatchSnapshot();
+});
