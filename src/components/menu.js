c27e954761d2c51219fef3828b2d11f1c8f11c70
krisbobo <kiyongbohbo@gmail.com>
2023-03-03 15:13:16 +0100

    Add browserrouter to fix test error

--- src/components/menu.js
+++ src/components/menu.js
@@ -1,20 +1,23 @@
-import { Link } from 'react-router-dom';
+import { BrowserRouter, Link, Outlet } from 'react-router-dom';
 import menuCss from './menu.module.css';
 
 const Menu = () => (
   <div className={menuCss.navContainer}>
-    <div className={menuCss.mathLogo}><Link to="/">Math magician</Link></div>
-    <ul className={menuCss.navbar}>
-      <li className="nav-item">
-        <Link to="/">Home</Link>
-      </li>
-      <li className="nav-item">
-        <Link to="/Calculator">Calculator</Link>
-      </li>
-      <li className="nav-item">
-        <Link to="/Quote">Quote</Link>
-      </li>
-    </ul>
+    <BrowserRouter>
+      <div className={menuCss.mathLogo}><Link to="/">Math magician</Link></div>
+      <ul className={menuCss.navbar}>
+        <li className="nav-item">
+          <Link to="/">Home</Link>
+        </li>
+        <li className="nav-item">
+          <Link to="/Calculator">Calculator</Link>
+        </li>
+        <li className="nav-item">
+          <Link to="/Quote">Quote</Link>
+        </li>
+      </ul>
+    </BrowserRouter>
+    <Outlet />
   </div>
 );
 
