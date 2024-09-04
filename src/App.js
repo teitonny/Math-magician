fc3eecedf64f92944fff51cac05000f6b277c7a2
tonnytech <tonnytei4@gmail.com>
2024-09-04 11:17:40 +0300

    Add a default route to the existing react routes

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import { Routes, Route } from 'react-router-dom';
+import { Routes, Route, Navigate } from 'react-router-dom';
 import './App.css';
 import Calculator from './components/Calculator';
 import Home from './components/Home';
@@ -14,6 +14,7 @@ function App() {
         <Route path="/" element={<Home />} />
         <Route path="/Calculator" element={<Calculator />} />
         <Route path="/Quote" element={<Quote />} />
+        <Route path="*" element={<Navigate to="/" />} />
       </Routes>
       <Footer />
     </>
